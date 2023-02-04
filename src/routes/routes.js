737037846f8c621efb0c7c618dc18601fb8fcf15
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-04 21:46:16 +0600

    Main

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,27 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main/Main";
+import Home from "../pages/Main/Home";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     children: [
-      //   {
-      //     path: "/",
-      //     element: <Home />,
-      //   },
-      //   {
-      //     path: "about",
-      //     element: <About />,
-      //   },
-      //   {
-      //     path: "top-rated",
-      //     element: <TopRated />,
-      //   },
-      //   {
-      //     path: "cart",
-      //     element: <Cart />,
-      //   },
+      {
+        path: "/",
+        element: <Home />,
+      },
     ],
   },
 ]);
