6d7ea3430e441a1b74c8c01a28b558a70cf78911
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-04 21:41:29 +0600

    Navbar design finished

--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -5,7 +5,7 @@ import Navbar from "../../pages/Shared/Navbar";
 
 const Main = () => {
   return (
-    <div className="px-10">
+    <div>
       <Navbar />
       <Outlet />
       <Footer />
--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,10 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsFillCartFill } from "react-icons/bs";
+import { IoIosListBox } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>this is navbar</h1>
-    </div>
+    <nav className="h-14 shadow-sm  m-2 max-w-7xl  px-5">
+      <ul className="h-full  mx-auto flex justify-between items-center gap-3 font-semibold text-indigo-900">
+        <h1 className="flex-1">
+          {" "}
+          <img
+            src="https://new.axilthemes.com/demo/template/blogar/assets/images/logo/logo-black.png"
+            alt="electro-House"
+            border="0"
+            className="h-10 "
+          />
+        </h1>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+        <li>
+          <Link to="/megamenu">Mega Menu</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li title="Wishlist" className="bg-red-500 p-2 rounded-full">
+          <Link to="/">
+            <IoIosListBox className="text-white" />
+          </Link>
+        </li>
+        <li title="cart" className="bg-red-500 p-2 rounded-full">
+          <Link to="/cart">
+            <BsFillCartFill className="text-white " />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
