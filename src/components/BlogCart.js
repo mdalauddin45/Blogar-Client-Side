2381f9e77ac1cce87cc76998eabbaed66036ab73
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-08 22:41:16 +0600

    cart design add

--- src/components/BlogCart.js
+++ src/components/BlogCart.js
@@ -1,60 +1,44 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 
-const BlogCart = () => {
-  const dispatch = useDispatch();
-  const { pathname } = useLocation();
+const BlogCart = ({ blog }) => {
   return (
-    <div className="shadow-lg relative rounded-3xl border p-3 flex flex-col text-indigo-900">
-      {pathname.includes("cart") && (
-        <div className="rounded-full grid place-items-center absolute top-2 right-2 bg-indigo-500 text-white h-8 w-8 font-bold ">
-          <p> {product.quantity} </p>
+    <div className="rounded-md shadow-md sm:w-96 ">
+      <div className="flex items-center justify-between p-3">
+        <div className="flex items-center space-x-2">
+          <img
+            src="https://source.unsplash.com/50x50/?portrait"
+            alt=""
+            className="object-cover object-center w-8 h-8 rounded-full shadow-sm dark:bg-gray-500 dark:border-gray-700"
+          />
+          <div className="-space-y-1">
+            <h2 className="text-sm font-semibold leading-none">
+              leroy_jenkins72
+            </h2>
+            <span className="inline-block text-xs leading-none dark:text-gray-400">
+              {blog?.category}
+            </span>
+          </div>
         </div>
-      )}
-      <div className="h-52 w-52 mx-auto">
-        <img src={product.image} alt={product.model} />
+        <button title="Open options" type="button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            className="w-5 h-5 fill-current"
+          >
+            <path d="M256,144a64,64,0,1,0-64-64A64.072,64.072,0,0,0,256,144Zm0-96a32,32,0,1,1-32,32A32.036,32.036,0,0,1,256,48Z"></path>
+            <path d="M256,368a64,64,0,1,0,64,64A64.072,64.072,0,0,0,256,368Zm0,96a32,32,0,1,1,32-32A32.036,32.036,0,0,1,256,464Z"></path>
+            <path d="M256,192a64,64,0,1,0,64,64A64.072,64.072,0,0,0,256,192Zm0,96a32,32,0,1,1,32-32A32.036,32.036,0,0,1,256,288Z"></path>
+          </svg>
+        </button>
       </div>
-      <h1 className="font-bold text-center">{product.model}</h1>
-      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
-      <div className=" flex-1">
-        <ul className="space-y-2">
-          {product.keyFeature.map((feature) => {
-            return (
-              <li key={feature} className="text-sm ">
-                {feature}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="flex gap-2 mt-5">
-        {!pathname.includes("cart") && (
-          <button
-            onClick={() => dispatch(addToCart(product))}
-            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-          >
-            Add to cart
-          </button>
-        )}
-
-        {!pathname.includes("cart") && (
-          <button
-            title="Add to wishlist"
-            className="bg-indigo-500  py-1 px-2 rounded-full"
-          >
-            <BiListPlus className="text-white" />
-          </button>
-        )}
-        {pathname.includes("cart") && (
-          <button
-            title="Remove"
-            onClick={() => dispatch(removeFromCart(product))}
-            className="flex justify-between px-3 bg-red-500 text-white p-1 rounded-full flex-1"
-          >
-            <p>Remove</p>
-            <MdDeleteForever size="25" />
-          </button>
-        )}
+      <img
+        src={blog?.image}
+        alt=""
+        className="object-cover object-center w-full h-72 dark:bg-gray-500"
+      />
+      <div className="p-3">
+        <h2 className="text-lg font-semibold leading-none">{blog?.title}</h2>
+        <p>{blog?.description?.slice(0, 100)}</p>
       </div>
     </div>
   );
--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BlogCart from "../../components/BlogCart";
 
 const Home = () => {
   const [blogs, setBlogs] = React.useState([]);
@@ -7,11 +8,13 @@ const Home = () => {
       .then((res) => res.json())
       .then((data) => setBlogs(data));
   }, []);
-  console.log(blogs);
+  // console.log(blogs);
 
   return (
-    <div>
-      <h1>this is home</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 py-10 px-10 lg:px-0 md:px-0">
+      {blogs?.map((blog, uxi) => (
+        <BlogCart key={uxi} blog={blog} />
+      ))}
     </div>
   );
 };
