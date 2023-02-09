e59da080673fe9a64b14311ed5afc144f04fcd30
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-09 21:21:35 +0600

    middleware newAction add

--- src/redux/middleWares/historyCounter.js
+++ src/redux/middleWares/historyCounter.js
@@ -1,8 +1,20 @@
 const historyCounter = (store) => (next) => (action) => {
-    console.log("Current State", store.getState());
-    console.log("Action", action);
-    return;
 
+    const history = store.getState().history.history;
+
+
+    if (action.type === "ADD_CONTENT") {
+
+        const newAction = {
+            ...action,
+            payload: {
+                ...action.payload, historyPosition: history.length
+            }
+        }
+
+        return next(newAction);
+    }
+    return next(action);
 }
 
 export default historyCounter;
