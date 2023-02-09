3e5e24ba692d6340b711405144ef1c2b32de9b94
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-09 22:26:14 +0600

    title name add

--- src/pages/Dashboard/AddBlog.js
+++ src/pages/Dashboard/AddBlog.js
@@ -6,19 +6,12 @@ const AddBlog = () => {
 
     const submit = (data) => {
         const product = {
-            model: data.model,
-            brand: data.brand,
-            status: data.status === "true" ? true : false,
-            price: data.price,
-            keyFeature: [
-                data.keyFeature1,
-                data.keyFeature2,
-                data.keyFeature3,
-                data.keyFeature4,
-            ],
-            spec: [],
+            title: data.title,
+            image: data.image,
+            category: data.category,
+            description: data.description,
+            data: new Date().toLocaleDateString(),
         };
-
         console.log(product);
     };
 
@@ -29,10 +22,10 @@ const AddBlog = () => {
                 onSubmit={handleSubmit(submit)}
             >
                 <div className='flex flex-col w-full max-w-xs'>
-                    <label className='mb-2' htmlFor='model'>
-                        Model
+                    <label className='mb-2' htmlFor='title'>
+                        Blog Title
                     </label>
-                    <input type='text' id='model' {...register("model")} />
+                    <input type='text' id='title' {...register("title")} />
                 </div>
                 <div className='flex flex-col w-full max-w-xs'>
                     <label className='mb-2' htmlFor='image'>
@@ -42,98 +35,31 @@ const AddBlog = () => {
                 </div>
 
                 <div className='flex flex-col w-full max-w-xs'>
-                    <label className='mb-3' htmlFor='brand'>
-                        Brand
+                    <label className='mb-3' htmlFor='category'>
+                        Category
                     </label>
-                    <select name='brand' id='brand' {...register("brand")}>
-                        <option value='amd'>AMD</option>
-                        <option value='intel'>Intel</option>
+                    <select name='category' id='category' {...register("category")}>
+                        <option value='tech'>Tech</option>
+                        <option value='apps'>Apps</option>
+                        <option value='media'>Media</option>
                     </select>
                 </div>
                 <div className='flex flex-col w-full max-w-xs'>
-                    <label className='mb-2' htmlFor='price'>
-                        Image
+                    <label className='mb-2' htmlFor='description'>
+                        Description
                     </label>
-                    <input type='text' name='price' id='price' {...register("price")} />
+                    <textarea
+                        className="w-full"
+                        type='textarea '
+                        name='description'
+                        id='description'
+                        {...register("description")}
+                        cols="30"
+                    ></textarea>
                 </div>
-
-                <div className='flex flex-col w-full max-w-xs'>
-                    <h1 className='mb-3'>Availability</h1>
-                    <div className='flex gap-3'>
-                        <div>
-                            <input
-                                type='radio'
-                                id='available'
-                                value={true}
-                                {...register("status")}
-                            />
-                            <label className='ml-2 text-lg' htmlFor='available'>
-                                Available
-                            </label>
-                        </div>
-                        <div>
-                            <input
-                                type='radio'
-                                id='stockOut'
-                                name='status'
-                                value={false}
-                                {...register("status")}
-                            />
-                            <label className='ml-2 text-lg' htmlFor='stockOut'>
-                                Stock out
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col w-full max-w-xs'></div>
-                <div className='flex flex-col w-full max-w-xs'>
-                    <label className='mb-2' htmlFor='keyFeature1'>
-                        Key Feature 1
-                    </label>
-                    <input
-                        type='text'
-                        name='keyFeature1'
-                        id='keyFeature1'
-                        {...register("keyFeature1")}
-                    />
-                </div>
-                <div className='flex flex-col w-full max-w-xs'>
-                    <label className='mb-2' htmlFor='keyFeature2'>
-                        Key Feature 2
-                    </label>
-                    <input
-                        type='text'
-                        name='keyFeature2'
-                        id='keyFeature2'
-                        {...register("keyFeature2")}
-                    />
-                </div>
-                <div className='flex flex-col w-full max-w-xs'>
-                    <label className='mb-2' htmlFor='keyFeature3'>
-                        Key Feature 3
-                    </label>
-                    <input
-                        type='text'
-                        name='keyFeature3'
-                        id='keyFeature3'
-                        {...register("keyFeature3")}
-                    />
-                </div>
-                <div className='flex flex-col w-full max-w-xs'>
-                    <label className='mb-2' htmlFor='keyFeature4'>
-                        Key Feature 4
-                    </label>
-                    <input
-                        type='text'
-                        name='keyFeature4'
-                        id='keyFeature4'
-                        {...register("keyFeature4")}
-                    />
-                </div>
-
                 <div className='flex justify-between items-center w-full'>
                     <button
-                        className=' px-4 py-3 bg-indigo-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500'
+                        className=' px-4 py-3 bg-red-500 rounded-md font-semibold text-white text-lg disabled:bg-gray-500'
                         type='submit'
                     >
                         Submit
